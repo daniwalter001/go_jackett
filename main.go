86ff4c428206371ff0874c7a62ba80427055f0c1
daniwalter001 <daniwalter001@gmail.com>
2024-05-09 15:00:41 +0000

    Add / route

--- main.go
+++ main.go
@@ -36,9 +36,9 @@ func main() {
 
 	app := fiber.New()
 
-	// app.Get("/", func(c *fiber.Ctx) error {
-	// 	return c.SendString("Working")
-	// })
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(200).SendString("Working")
+	})
 
 	app.Get("/manifest.json", func(c *fiber.Ctx) error {
 		a := types.StreamManifest{ID: "strem.go.beta", Description: "Random Golang version on stremio Addon", Name: "GoDon", Resources: []string{"stream"}, Version: "1.0.9", Types: []string{"movie", "series", "anime"}, Logo: "https://upload.wikimedia.org/wikipedia/commons/2/23/Golang.png"}
