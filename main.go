dff2562f4defabbd7d28fdfda19a7774eadf1faf
daniwalter001 <daniwalter001@gmail.com>
2024-05-10 13:16:25 +0000

    Add port in .env

--- main.go
+++ main.go
@@ -412,5 +412,11 @@ func main() {
 		return c.Status(fiber.StatusOK).JSON(ttttt)
 	})
 
-	app.Listen(":8000")
+	port := "3000"
+
+	if os.Getenv("PORT") != "" {
+		port = os.Getenv("PORT")
+	}
+
+	app.Listen(fmt.Sprintf(":%s", port))
 }
