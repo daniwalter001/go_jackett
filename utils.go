08bd3babce6b8ad85bb1334a54e24a1b743b2085
daniwalter001 <daniwalter001@gmail.com>
2024-05-11 16:50:58 +0000

    Maj nyaasi servers lists

--- utils.go
+++ utils.go
@@ -220,7 +220,7 @@ func readTorrent(item types.ItemsParsed) types.ItemsParsed {
 	torrentFile, _ := gotorrentparser.Parse(fileReader)
 
 	for _, file := range torrentFile.Files {
-		fmt.Println(file.Path[len(file.Path)-1])
+		// fmt.Println(file.Path[len(file.Path)-1])
 
 		files = append(files, types.TorrentFile{
 			Name:         file.Path[len(file.Path)-1],
@@ -239,8 +239,6 @@ func readTorrent(item types.ItemsParsed) types.ItemsParsed {
 }
 
 func readTorrentFromMagnet(item types.ItemsParsed) types.ItemsParsed {
-
-	// fmt.Println(item.Link)
 
 	c := TorrentClient()
 
