a31d8097423da7a0e5cf25f50b345b143dddc2c5
daniwalter001 <daniwalter001@gmail.com>
2024-07-08 17:48:04 +0000

    %d

--- redis.go
+++ redis.go
@@ -12,8 +12,7 @@ func RedisClient() redis.Client {
 
 	errDot := godotenv.Load("./.env")
 	if errDot != nil {
-		fmt.Println("Error loading .env file")
-		panic(errDot)
+		fmt.Println("Error loading .env file...pass")
 	}
 
 	opt, _ := redis.ParseURL(fmt.Sprintf("rediss://default:%s@%s:%s/0?max_retries=2", os.Getenv("REDIS_PASSWORD"), os.Getenv("REDIS_URL"), os.Getenv("REDIS_PORT")))
